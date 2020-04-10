c2d7c20b9c2a4028c1132708458438f5e14c859d
siddeshshewde <siddesh.darkeyed95@gmail.com>
2020-04-11 02:06:30 +0530

    Add files via upload

---
+++ Codewars/Kata/8 Kyu/Number toString/tostring.java
@@ -0,0 +1,17 @@
+/*
+Title:
+	Number toString
+
+Kata:
+	https://www.codewars.com/kata/53934feec44762736c00044b/train/java
+
+Description:
+	The code gives an error!
+
+	public static final String a = 123.toString();
+	Fix it!
+*/
+
+public class ToString {
+  	public static final String a = String.valueOf(123);
+}
---
+++ Codewars/Kata/8 Kyu/Number toString/tostring_test.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ToStringTest {
+  @Test
+  public void tests() {
+    assertEquals("Wrong!", ToString.a, "123");
+  }
+}
