a9b02c517765603d42392659923112d02a8af85e
siddeshshewde <siddesh.darkeyed95@gmail.com>
2020-05-25 19:08:54 +0530

    Update Day01.java

--- Advent Of Code/2015/Day01/Day01.java
+++ Advent Of Code/2015/Day01/Day01.java
@@ -6,7 +6,7 @@ Title:
 Link:
     https://adventofcode.com/2015/day/1#
 
-Description:
+Description Part1:
    Santa is trying to deliver presents in a large apartment building, but he can't find the right floor - 
    the directions he got are a little confusing. He starts on the ground floor (floor 0) and then follows the 
    instructions one character at a time.
@@ -16,7 +16,7 @@ Description:
 
     The apartment building is very tall, and the basement is very deep; he will never find the top or bottom floors.
 
-    To what floor do the instructions take Santa?
+    To what floor do the instructions take Santa? What is the position of the character that causes Santa to first enter the basement?
 
 Example:
     (()) and ()() both result in floor 0.
@@ -24,6 +24,15 @@ Example:
     ))((((( also results in floor 3.
     ()) and ))( both result in floor -1 (the first basement level).
     ))) and )())()) both result in floor -3.
+
+Description Part2:
+    Now, given the same instructions, find the position of the first character that causes him to enter the basement (floor -1). 
+    The first character in the instructions has position 1, the second character has position 2, and so on.
+    What is the position of the character that causes Santa to first enter the basement?
+
+Example:
+    ) causes him to enter the basement at character position 1.
+    ()()) causes him to enter the basement at character position 5.
 
 Puzzle Input:
     https://adventofcode.com/2015/day/1/input
