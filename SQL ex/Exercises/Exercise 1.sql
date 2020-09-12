60707a41a87b7e28e9fa5a997fb0269405bef1d6
sid-jOkEr <siddesh.shewde@gmail.com>
2020-09-12 19:10:18 +0530

    SQL ex Exercise 1

---
+++ SQL ex/Exercises/Exercise 1.sql
@@ -0,0 +1,19 @@
+/*
+Exercise : 1
+User     : Serge I
+Date     : 2002-09-30
+
+Problem
+Find the model number, speed and hard drive capacity for all the PCs with prices below $500.
+Result set: model, speed, hd.
+
+Help Topics
+Simple Select Statement - http://www.sql-tutorial.ru/en/book_simple_select_statement/page1.html
+
+*/
+
+select a.model, b.speed, b.hd
+from product a
+left join pc b
+on a.model = b.model
+where price < 500
---
+++ SQL ex/README.md
@@ -0,0 +1,6 @@
+<h1> SQL ex Solutions</h1>
+
+Website: https://sql-ex.ru/
+
+Username: Siddddd
+
