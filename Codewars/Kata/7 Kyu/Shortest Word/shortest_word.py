e9c0211a7fc6e4da0f735378ced2a031bdc831e4
siddeshshewde <siddesh.shewde@gmail.com>
2021-03-25 13:24:32 -0400

    Add files via upload

---
+++ Codewars/Kata/7 Kyu/Shortest Word/shortest_word.py
@@ -0,0 +1,13 @@
+'''
+Problem     : Shortest Word
+Difficulty  : 7 Kyu
+URL         : https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9/train/python
+'''
+
+def find_short(s):
+    s = s.split()
+    l = len(s[0])
+    for i in s:
+        if len(i) < l:
+            l = len(i)
+    return l
