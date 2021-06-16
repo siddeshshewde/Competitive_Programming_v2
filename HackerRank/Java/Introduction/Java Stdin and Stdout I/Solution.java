1a2f031d0ae43b7ea3b777fdbaa29629a13ceaab
siddeshshewde <siddesh.shewde@gmail.com>
2021-06-16 17:43:19 -0400

    Add files via upload

---
+++ HackerRank/Java/Introduction/Java Stdin and Stdout I/Solution.java
@@ -0,0 +1,21 @@
+/*
+Problem     : Java Stdin and Stdout I
+Difficulty  : Easy
+Max Score   : 5
+URL         : https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem
+*/
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        System.out.println(a);
+        a = scan.nextInt();
+        System.out.println(a);
+        a = scan.nextInt();
+        System.out.println(a);
+    }
+}
