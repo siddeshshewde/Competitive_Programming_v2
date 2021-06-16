c8e467a33de3055491ce3377561b7c12b8119e8f
siddeshshewde <siddesh.shewde@gmail.com>
2021-06-16 14:54:33 -0400

    Add files via upload

---
+++ HackerRank/Java/Introduction/Welcome to Java!/Solution.java
@@ -0,0 +1,14 @@
+/*
+Problem     : Quarter of the year
+Difficulty  : Easy
+Max Score   : 3
+URL         : https://www.hackerrank.com/challenges/welcome-to-java/problem
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
