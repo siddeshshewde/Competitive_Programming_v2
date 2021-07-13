84f3d4ccb91996f1fb8d3ba4688a1cdbf19ab3f3
siddeshshewde <siddesh.shewde@gmail.com>
2021-07-13 19:11:22 -0400

    Add files via upload

---
+++ Codewars/Kata/8 Kyu/Return Negative/Solution.py
@@ -0,0 +1,10 @@
+'''
+Problem     : Return Negative
+Difficulty  : 8 Kyu
+URL         : https://www.codewars.com/kata/55685cd7ad70877c23000102/train/python
+'''
+
+def make_negative( number ):
+    if number > 0:
+        return number * (-1)
+    return number
