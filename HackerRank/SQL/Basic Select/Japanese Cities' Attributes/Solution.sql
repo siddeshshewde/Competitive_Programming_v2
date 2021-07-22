c35d43ef0b95c15d8db0cb10447dc479da78732b
siddeshshewde <siddesh.shewde@gmail.com>
2021-07-22 02:39:36 -0400

    Add files via upload

---
+++ HackerRank/SQL/Basic Select/Japanese Cities' Attributes/Solution.sql
@@ -0,0 +1,10 @@
+/*
+Problem     : Japanese Cities' Attributes
+Difficulty  : Easy
+Max Score   : 10
+URL         : https://www.hackerrank.com/challenges/japanese-cities-attributes/problem
+*/
+
+SELECT *
+FROM CITY
+WHERE COUNTRYCODE = 'JPN';
