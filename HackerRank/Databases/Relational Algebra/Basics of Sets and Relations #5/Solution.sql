6d36d761b2c851d84fe8cb7659a3e9b253bde1f6
siddeshshewde <siddesh.shewde@gmail.com>
2021-07-22 02:35:40 -0400

    Add files via upload

---
+++ HackerRank/Databases/Relational Algebra/Basics of Sets and Relations #5/Solution.sql
@@ -0,0 +1,8 @@
+/*
+Problem     : Basics of Sets and Relations #5
+Difficulty  : Easy
+Max Score   : 5
+URL         : https://www.hackerrank.com/challenges/basics-of-sets-and-relational-algebra-5/problem
+*/
+
+2
