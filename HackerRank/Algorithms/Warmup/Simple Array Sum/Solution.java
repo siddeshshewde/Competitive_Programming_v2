1ffec4947bc94a42a8a00542466d8a7dbe543d9d
siddeshshewde <siddesh.shewde@gmail.com>
2021-07-22 02:31:12 -0400

    Add files via upload

---
+++ HackerRank/Algorithms/Warmup/Simple Array Sum/Solution.java
@@ -0,0 +1,61 @@
+/*
+Problem     : Simple Array Sum
+Difficulty  : Easy
+Max Score   : 10
+URL         : https://www.hackerrank.com/challenges/simple-array-sum/problem
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'simpleArraySum' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY ar as parameter.
+     */
+
+    public static int simpleArraySum(List<Integer> ar) {
+        int n = 0;
+        for (int i = 0 ; i < ar.size() ; i++)
+        {
+            n += ar.get(i);
+        }
+        return n;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] arTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> ar = new ArrayList<>();
+
+        for (int i = 0; i < arCount; i++) {
+            int arItem = Integer.parseInt(arTemp[i]);
+            ar.add(arItem);
+        }
+
+        int result = Result.simpleArraySum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
