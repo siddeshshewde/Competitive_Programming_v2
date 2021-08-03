711781a95fc1547c0f56d6cdd17c870ad3d176df
siddeshshewde <siddesh.shewde@gmail.com>
2021-08-03 13:00:03 -0400

    Add files via upload

---
+++ HackerRank/SQL/Basic Select/Japanese Cities' Names/Solution.sql
@@ -0,0 +1,10 @@
+/*
+Problem     : Japanese Cities' Names
+Difficulty  : Easy
+Max Score   : 10
+URL         : https://www.hackerrank.com/challenges/japanese-cities-name/problem
+*/
+
+SELECT Name
+FROM CITY
+WHERE COUNTRYCODE = 'JPN';
