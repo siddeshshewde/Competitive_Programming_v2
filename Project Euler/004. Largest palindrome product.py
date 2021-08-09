584ad5e3991a6464c1e7711e281f125537630475
siddeshshewde <siddesh.shewde@gmail.com>
2021-08-09 15:49:17 -0400

    Add files via upload

---
+++ Project Euler/004. Largest palindrome product.py
@@ -0,0 +1,11 @@
+def main():
+    max = 0
+    while i in range(999, 1):
+        while j in range(999, 1):
+            if palindrome_check(i * j):
+                if i * j > max:
+                    max = i * j
+    return max
+
+def palindrome_check(n):
+    return n == int("".join(reversed(str(n))))
