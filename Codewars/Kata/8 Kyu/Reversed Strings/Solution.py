466c993eb028f4042c9d8824de7243961e012239
siddeshshewde <siddesh.shewde@gmail.com>
2021-08-22 15:06:18 -0400

    Add files via upload

---
+++ Codewars/Kata/8 Kyu/Reversed Strings/Solution.py
@@ -0,0 +1,8 @@
+'''
+Problem     : Reversed Strings
+Difficulty  : 8 Kyu
+URL         : https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/python
+'''
+
+def solution(string):
+    return "".join(reversed(string))
