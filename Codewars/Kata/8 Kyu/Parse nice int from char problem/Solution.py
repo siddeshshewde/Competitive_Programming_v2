957140d40d81feee36099c8ee8690ac8b3173a64
siddeshshewde <siddesh.shewde@gmail.com>
2021-08-24 15:19:58 -0400

    Add files via upload

---
+++ Codewars/Kata/8 Kyu/Parse nice int from char problem/Solution.py
@@ -0,0 +1,8 @@
+'''
+Problem     : Parse nice int from char problem
+Difficulty  : 8 Kyu
+URL         : https://www.codewars.com/kata/557cd6882bfa3c8a9f0000c1/train/python
+'''
+
+def get_age(age):
+    return int(age[0])
