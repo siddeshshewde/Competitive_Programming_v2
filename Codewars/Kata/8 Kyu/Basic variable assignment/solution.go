d56c4b937a514b42f35a4607e9969df57cafde4b
siddeshshewde <siddesh.shewde@gmail.com>
2021-12-05 23:35:26 -0500

    Add files via upload

---
+++ Codewars/Kata/8 Kyu/Basic variable assignment/solution.go
@@ -0,0 +1,13 @@
+/*
+Problem     : Basic variable assignment
+Difficulty  : 8 Kyu
+URL         : https://www.codewars.com/kata/50ee6b0bdeab583673000025/train/go
+*/
+
+package kata
+func Namevar() string {
+  var a string = "code"
+  var b string = "wa.rs"
+  var name string = a + b
+  return name
+}
