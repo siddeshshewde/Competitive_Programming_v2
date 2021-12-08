7b742d092d0e55c6f8755a763551a95c71c7bbc0
siddeshshewde <siddesh.shewde@gmail.com>
2021-12-07 19:50:05 -0500

    Add files via upload

---
+++ SQL ex/Exercises/Exercise 4.sql
@@ -0,0 +1,14 @@
+/*
+Exercise : 4
+User     : Serge I
+Date     : 2002-09-21
+Link     : https://sql-ex.ru/learn_exercises.php?LN=4
+Problem
+Find all records from the Printer table containing data about color printers.
+Help topic
+Simple SELECT statement - http://www.sql-tutorial.ru/en/book_simple_select_statement/page1.html
+*/
+
+SELECT * 
+FROM Printer
+WHERE color = 'y';
