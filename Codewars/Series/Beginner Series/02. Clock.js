79b6ccec894e08f18446ee6f34af8efd076b9432
siddeshshewde <siddesh.shewde@gmail.com>
2021-12-14 17:32:26 -0500

    Add files via upload

---
+++ Codewars/Series/Beginner Series/02. Clock.js
@@ -0,0 +1,9 @@
+/*
+Problem     : Beginner Series #2 Clock
+Difficulty  : 8 Kyu
+URL         : https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/javascript
+*/
+
+function past(h, m, s){
+    return h*3600000+m*60000+s*1000;
+}
