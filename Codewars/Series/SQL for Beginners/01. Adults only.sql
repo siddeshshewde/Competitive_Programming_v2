460f65338333609cf0536514edc95c0fd29da409
siddeshshewde <siddesh.shewde@gmail.com>
2021-12-14 18:23:28 -0500

    Add files via upload

---
+++ Codewars/Series/SQL for Beginners/01. Adults only.sql
@@ -0,0 +1,9 @@
+/*
+Problem     : Adults only (SQL for Beginners #1)
+Difficulty  : 8 Kyu
+URL         : https://www.codewars.com/kata/590a95eede09f87472000213/train/sql
+*/
+
+SELECT name, age
+FROM users
+where age > 17;
