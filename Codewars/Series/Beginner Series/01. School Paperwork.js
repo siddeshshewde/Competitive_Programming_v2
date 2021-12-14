2f1f4b523ab04bd003f284670c7de5a1bc7b9c01
siddeshshewde <siddesh.shewde@gmail.com>
2021-12-14 17:28:07 -0500

    Add files via upload

---
+++ Codewars/Series/Beginner Series/01. School Paperwork.js
@@ -0,0 +1,9 @@
+/*
+Problem     : Beginner Series #1 School Paperwork
+Difficulty  : 8 Kyu
+URL         : https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd/train/javascript
+*/
+
+function paperwork(n, m) {
+    return n > 0 && m > 0 ? m * n : 0;
+}
