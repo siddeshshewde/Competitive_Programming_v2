85766c995f0ceee4cdafd4ad4d9fe951b5f212f9
siddeshshewde <siddesh.shewde@gmail.com>
2021-12-14 20:29:19 -0500

    Add files via upload

---
+++ Codewars/Series/All Star Code Challenge/1. All Star Code Challenge.py
@@ -0,0 +1,8 @@
+'''
+Problem     : All Star Code Challenge #1
+Difficulty  : 7 Kyu
+URL         : https://www.codewars.com/kata/5863f97fb3a675d9a700003f/train/python
+'''
+
+def sum_ppg(player_one, player_two):
+    return player_one['ppg'] + player_two['ppg']
