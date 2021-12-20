95c2b03fb10c29bf9d6b33db8c3cff55997bef12
siddeshshewde <siddesh.shewde@gmail.com>
2021-12-20 03:20:34 -0500

    Update WeirdAlgorithm.java

--- CSES/Problem Set/WeirdAlgorithm.java
+++ CSES/Problem Set/WeirdAlgorithm.java
@@ -5,31 +5,98 @@ Time limit   : 1.00 s
 Memory limit : 512 MB
 */
 
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class WeirdAlgorithm
 {
-    public static void main(String args[])
-    {
-        Scanner sc = new Scanner(System.in);
+    PrintWriter out;
+    StringTokenizer st;
+    BufferedReader br;
+    final int imax = Integer.MAX_VALUE, imin = Integer.MIN_VALUE;
+    final int mod = 1000000007;
 
-        double n = sc.nextDouble();
-        sc.close();
-        System.out.printf("%.0f ", n);
-        
+    void solve() throws Exception {
+        read();
+        long n= ni();
+
         while (n > 1)
         {
-            if (n % 2 == 0)
-            {
-                n /= 2;
-                System.out.printf("%.0f ", n);
-            }
-            else
-            {
-                n = n * 3 + 1;
-                System.out.printf("%.0f ", n);
-            }
+            out.print(n + " ");
+            n = (n % 2 == 0)? n/2 : n * 3 +1;
         }
-        return;
+        out.println(1);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new WeirdAlgorithm().run();
+    }
+
+    void run() throws Exception {
+        out = new PrintWriter(System.out);
+        br = new BufferedReader(new InputStreamReader(System.in));
+
+        solve();
+        out.flush();
+    }
+
+    void read() throws Exception {
+        st = new StringTokenizer(br.readLine());
+    }
+
+    int ni() {
+        return Integer.parseInt(st.nextToken());
+    }
+
+    long nl() {
+        return Long.parseLong(st.nextToken());
+    }
+
+    double nd() {
+        return Double.parseDouble(st.nextToken());
+    }
+
+    String ns() throws Exception {
+        String s = br.readLine();
+        return s.length() == 0 ? br.readLine() : s;
+    }
+
+    void print(int[] arr) {
+        for (int i : arr)
+            out.print(i + " ");
+        out.println();
+    }
+
+    void print(long[] arr) {
+        for (long i : arr)
+            out.print(i + " ");
+        out.println();
+    }
+
+    void print(int[][] arr) {
+        for (int[] i : arr) {
+            for (int j : i)
+                out.print(j + " ");
+            out.println();
+        }
+    }
+
+    void print(long[][] arr) {
+        for (long[] i : arr) {
+            for (long j : i)
+                out.print(j + " ");
+            out.println();
+        }
+    }
+
+    long add(long a, long b) {
+        if (a + b >= mod)
+            return (a + b) - mod;
+        else
+            return a + b;
+    }
+
+    long mul(long a, long b) {
+        return (a * b) % mod;
     }
 }
