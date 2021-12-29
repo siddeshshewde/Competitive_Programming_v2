74f0e2d4067806aecd695392d6aaff7cd7c7bea7
siddeshshewde <siddesh.shewde@gmail.com>
2021-12-29 02:12:54 -0500

    Add files via upload

---
+++ CSES/Problem Set/Repetitions.java
@@ -0,0 +1,101 @@
+/*
+Title        : Repetitions
+Problem      : https://cses.fi/problemset/task/1069
+Category     : Introductory Problems
+Time limit   : 1.00 s 
+Memory limit : 512 MB
+*/
+
+import java.util.*;
+import java.io.*;
+
+public class Repetitions
+{
+    PrintWriter out;
+    StringTokenizer st;
+    BufferedReader br;
+    final int imax = Integer.MAX_VALUE, imin = Integer.MIN_VALUE;
+    final int mod = 1000000007;
+
+    void solve() throws Exception {
+        String s = ns();
+        String input[] = s.split("");
+        int max = 1, temp = 1;
+        String prev = input[0];
+
+        for (int i = 1; i < input.length ; i++)
+        {
+            if (input[i].equals(prev))
+                temp++;
+            else
+            {
+                temp = 1;
+                prev = input[i];
+            }
+            if (temp > max)
+                max = temp;
+        }
+        out.print(max);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Repetitions().run();
+    }
+
+    void run() throws Exception {
+        out = new PrintWriter(System.out);
+        br = new BufferedReader(new InputStreamReader(System.in));
+
+        solve();
+        out.flush();
+    }
+
+    void read() throws Exception {
+        st = new StringTokenizer(br.readLine());
+    }
+
+    int ni() {
+        return Integer.parseInt(st.nextToken());
+    }
+
+    long nl() {
+        return Long.parseLong(st.nextToken());
+    }
+
+    double nd() {
+        return Double.parseDouble(st.nextToken());
+    }
+
+    String ns() throws Exception {
+        String s = br.readLine();
+        return s.length() == 0 ? br.readLine() : s;
+    }
+
+    void print(int[] arr) {
+        for (int i : arr)
+            out.print(i + " ");
+        out.println();
+    }
+
+    void print(long[] arr) {
+        for (long i : arr)
+            out.print(i + " ");
+        out.println();
+    }
+
+    void print(int[][] arr) {
+        for (int[] i : arr) {
+            for (int j : i)
+                out.print(j + " ");
+            out.println();
+        }
+    }
+
+    void print(long[][] arr) {
+        for (long[] i : arr) {
+            for (long j : i)
+                out.print(j + " ");
+            out.println();
+        }
+    }
+}
