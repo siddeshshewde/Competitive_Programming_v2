0874ae68dff73f572eebbfe6659421331f5dcd4c
siddeshshewde <siddesh.shewde@gmail.com>
2021-12-29 02:17:59 -0500

    Add files via upload

---
+++ Codecombat/Dungeon/Kithguard Dungeon/True Names.js
@@ -0,0 +1,13 @@
+//https://codecombat.com/play/level/true-names?
+
+// Defend against "Brak" and "Treg"!
+// You must attack small ogres twice.
+
+hero.moveRight();
+hero.attack("Brak");
+hero.attack("Brak");
+hero.moveRight();
+hero.moveRight();
+hero.attack("Treg");
+hero.attack("Treg");
+hero.moveRight();
