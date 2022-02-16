42a2eb1ce32f840e4cd724faf3a16902aabe14bc
siddeshshewde <siddesh.shewde@gmail.com>
2022-02-16 11:25:04 -0500

    Add files via upload

---
+++ Nados/Programming/DSA/DSA Level 1/Basics Of Programming/Getting Started/Print Z.java
@@ -0,0 +1,17 @@
+/*
+Problem     : Print Z
+Difficulty  : Easy
+Description : https://nados.io/question/print-z
+Score       : 10/10
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("*****");
+        System.out.println("   * ");
+        System.out.println("  *  ");
+        System.out.println(" *   ");
+        System.out.println("*****");
+    }
+}
