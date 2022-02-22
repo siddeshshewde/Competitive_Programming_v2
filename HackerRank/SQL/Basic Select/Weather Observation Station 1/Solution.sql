cc983e29937f88caea29de257deea20d15637b3d
siddeshshewde <siddesh.shewde@gmail.com>
2022-02-22 03:47:17 -0500

    Add files via upload

---
+++ HackerRank/SQL/Basic Select/Weather Observation Station 1/Solution.sql
@@ -0,0 +1,9 @@
+/*
+Problem     : Weather Observation Station 1
+Difficulty  : Easy
+Max Score   : 15
+URL         : https://www.hackerrank.com/challenges/weather-observation-station-1/problem
+*/
+
+SELECT CITY, STATE
+FROM STATION;
