a5e2f55d81fdf73a2b46547c7df066d1ee1ab54d
siddeshshewde <siddesh.shewde@gmail.com>
2022-02-28 03:47:57 -0500

    Update problem001.py

--- Daily Coding Problem/Solutions/problem001.py
+++ Daily Coding Problem/Solutions/problem001.py
@@ -1,3 +1,13 @@
+"""
+This problem was recently asked by Google.
+
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+Bonus: Can you do this in one pass?
+"""
+
 def check_sum(arr, target):
     potential_solutions = []
     
@@ -14,3 +24,9 @@ print (check_sum([10, 15, 3, 7], 19)) == False
 print (check_sum([10, 15, 3, 4], 17)) == False
 print (check_sum([], 17))             == False
 print (check_sum([10, 15, 3, 7],17))  == True
+
+"""
+SPECS:
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(n)
+"""
