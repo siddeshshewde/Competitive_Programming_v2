d148afe08695d4a869786f463cd787d6b255f1cf
siddeshshewde <siddesh.shewde@gmail.com>
2022-02-28 03:58:41 -0500

    Add files via upload

---
+++ Daily Coding Problem/Solutions/problem005.py
@@ -0,0 +1,52 @@
+"""
+This problem was asked by Jane Street.
+
+cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair. For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+
+Given this implementation of cons:
+
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+Implement car and cdr.
+"""
+
+from typing import Callable
+
+# given implementation of cons:
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+
+    return pair
+
+# car implementation
+def car(f: Callable) -> int:
+    z = lambda x, y: x
+    return f(z)
+
+
+# cdr implementation
+def cdr(f: Callable) -> int:
+    z = lambda x, y: y
+    return f(z)
+
+
+if __name__ == "__main__":
+    pair = cons(1, 3)
+
+    print(car(pair))
+    print(cdr(pair))
+
+"""
+SPECS:
+
+car:
+TIME COMPLEXITY: O(1) 
+SPACE COMPLEXITY: O(1)
+
+cdr:
+TIME COMPLEXITY: O(1) 
+SPACE COMPLEXITY: O(1)
+"""
