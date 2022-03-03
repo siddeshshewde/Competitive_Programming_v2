0cbb672d95c75073e98948a42b49c66b1cc07127
siddeshshewde <siddesh.shewde@gmail.com>
2022-03-03 04:13:01 -0500

    Add files via upload

---
+++ SQLBolt/SQL Lesson 18 - Dropping tables.sql
@@ -0,0 +1,10 @@
+/*
+Name: SQL Lesson 18: Dropping tables
+Link: https://sqlbolt.com/lesson/dropping_tables
+*/
+
+1. We've sadly reached the end of our lessons, lets clean up by removing the Movies table ✓
+DROP table movies;
+
+2. And drop the BoxOffice table as well ✓
+drop table boxoffice;
