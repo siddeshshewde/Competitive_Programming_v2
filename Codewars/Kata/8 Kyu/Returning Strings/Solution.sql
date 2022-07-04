2caa83cb66e37dbf36a1e26465580e841d7933ba
siddeshshewde <siddesh.shewde@gmail.com>
2022-07-04 17:52:47 -0400

    Add files via upload

---
+++ Codewars/Kata/8 Kyu/Returning Strings/Solution.sql
@@ -0,0 +1,8 @@
+/*
+Name: Returning Strings
+Link: https://www.codewars.com/kata/55a70521798b14d4750000a4/train/sql
+Difficulty: 8 Kyu
+*/
+
+SELECT CONCAT('Hello, ', name, ' how are you doing today?') as greeting
+FROM person;
