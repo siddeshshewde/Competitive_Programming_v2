ac6bbb2366f1c4a149ea38dfd0ddd7eb4e253172
siddeshshewde <siddesh.shewde@gmail.com>
2022-07-06 14:46:59 -0400

    Add files via upload

---
+++ Leetcode/Problems/0217. Contains Duplicate.java
@@ -0,0 +1,25 @@
+/*
+Problem     : Contains Duplicate
+Difficulty  : Easy
+URL         : https://leetcode.com/problems/contains-duplicate/
+*/
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> num = new HashSet<Integer>();
+        for (int i = 0 ; i < nums.length ; i++)
+        {
+            num.add(nums[i]);
+        }
+        if (num.size() < nums.length)
+        {
+            return true;
+        }
+        return false;
+    }
+}
+
+/*
+Runtime: 29 ms, faster than 12.95% of Java online submissions for Contains Duplicate.
+Memory Usage: 71.8 MB, less than 16.51% of Java online submissions for Contains Duplicate.
+*/
