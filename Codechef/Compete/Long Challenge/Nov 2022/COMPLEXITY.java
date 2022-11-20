8e64c16c96b57ef5e505f930bfc2ece5414cd2b8
siddeshshewde <siddesh.shewde@gmail.com>
2022-11-20 13:36:54 -0500

    Add files via upload

---
+++ Codechef/Compete/Long Challenge/Nov 2022/COMPLEXITY.java
@@ -0,0 +1,26 @@
+/*
+Problem: COMPLEXITY
+Contest: November Long
+Link: https://www.codechef.com/problems/COMPLEXITY
+*/
+
+import java.util.Scanner;
+
+class codechef
+{
+    public static void main (String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for ( int i = 1 ; i <= t ; i++)
+        {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if (a > b)
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
+    }
+}
