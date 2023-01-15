5930d59bc163889cc89be455a585b0c2a2700fa5
siddeshshewde <siddesh.shewde@gmail.com>
2023-01-15 16:26:22 -0500

    Add files via upload

---
+++ Codewars/Kata/7 Kyu/String ends with/Solution.py
@@ -0,0 +1,8 @@
+'''
+Problem     : String ends with?
+Difficulty  : 7 Kyu
+URL         : https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d/train/python
+'''
+
+def solution(text, ending):
+    return text.endswith(ending)
