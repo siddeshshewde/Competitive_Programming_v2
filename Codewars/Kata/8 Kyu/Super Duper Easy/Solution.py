2515d1a75bf323bf51e93127f7b48c1ad4d9073b
siddeshshewde <siddesh.shewde@gmail.com>
2023-01-24 22:03:46 -0500

    Add files via upload

---
+++ Codewars/Kata/8 Kyu/Super Duper Easy/Solution.py
@@ -0,0 +1,10 @@
+'''
+Problem     : Super Duper Easy
+Difficulty  : 8 Kyu
+URL         : https://www.codewars.com/kata/55a5bfaa756cfede78000026/train/python
+'''
+
+def problem(a):
+    if type(a) == str:
+        return "Error"
+    return a*50+6
