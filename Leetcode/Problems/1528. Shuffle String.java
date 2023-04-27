95743eca76e8db86e7ee801435fd7c07173f36bd
siddeshshewde <siddesh.shewde@gmail.com>
2023-04-27 17:44:56 -0400

    Add files via upload

---
+++ Leetcode/Problems/1528. Shuffle String.java
@@ -0,0 +1,13 @@
+import java.lang.String;
+
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        String a[];
+
+        for (int i = 0 ; i < indices.length ; i++)
+        {
+            a[indices[i]] = s.charAt(i);
+        }
+        return new String(a);
+    }
+}
