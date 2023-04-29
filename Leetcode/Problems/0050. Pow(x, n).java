44d66d61a2769052de45865d5846723047dfb531
siddeshshewde <siddesh.shewde@gmail.com>
2023-04-29 00:29:56 -0400

    Add files via upload

---
+++ Leetcode/Problems/0050. Pow(x, n).java
@@ -0,0 +1,17 @@
+/*
+Problem     : Pow(x, n)
+URL         : https://leetcode.com/problems/powx-n/
+*/
+
+class Solution {
+    public double myPow(double x, int n) {
+        return Math.pow(x, n);
+    }
+}
+
+/*
+Runtime: 0 ms
+Beats: 100%
+Memory: 40.7 MB
+Beats: 79.73%
+*/
