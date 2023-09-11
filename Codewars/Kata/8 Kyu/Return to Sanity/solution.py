f1785d0c7d9dd7a7529d32130348bf9929d9f495
siddeshshewde <siddesh.shewde@gmail.com>
2023-09-11 15:31:29 -0400

    Add files via upload

---
+++ Codewars/Kata/8 Kyu/Return to Sanity/solution.py
@@ -0,0 +1,8 @@
+'''
+Problem     : Convert a Number to a String!
+Difficulty  : 8 Kyu
+URL         : https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/python
+'''
+
+def number_to_string(num):
+    return str(num)
