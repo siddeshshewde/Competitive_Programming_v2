7284688559af70a315974fe8b2cbb6955536c541
siddeshshewde <siddesh.shewde@gmail.com>
2023-09-21 11:40:01 -0400

    Add files via upload

---
+++ Codewars/Kata/8 Kyu/Function 2 - squaring an argument/solution.py
@@ -0,0 +1,8 @@
+'''
+Problem     : Function 2 - squaring an argument
+Difficulty  : 8 Kyu
+URL         : https://www.codewars.com/kata/523b623152af8a30c6000027/train/python
+'''
+
+def square(n):
+    return n*n
